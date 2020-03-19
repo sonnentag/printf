4959d7b14a43f700b1a6773e73fb5867deca7e89
Tony Sims <zocle@zocle.net>
2020-03-19 03:52:14 +0000

    moved struct array to separate function and fixed 3 failed chacks

--- _printf.c
+++ _printf.c
@@ -11,33 +11,34 @@
  */
 int _printf(const char *format, ...)
 {
-	int j, n = 0, i = 0;
+	int x = 0, n = 0, i = 0;
 	va_list args;
-	spec_t specs[] = {
-		{"c", s_c},
-		{"s", s_s},
-		{"i", s_i},
-		{"d", s_d},
-		{"%", s_z}
-	};
 
 	if (format)
 	{
 		va_start(args, format);
 		while (format[i] != '\0')
 		{
-			j = 0;
 			if (format[i] == '%')
 			{
 				i++;
-				while (j < 5)
+				if (!format[i])
+					return (-1);
+
+				if (format[i] == '%')
+					n += _putchar(format[i]);
+				else
 				{
-					if (specs[j].spec[0] == format[i])
+					x = get_spec(format[i], args);
+					if (x > 0)
+						n += x;
+					else
 					{
-						n += specs[j].f(args);
+						n += _putchar(format[i - 1]);
+						n += _putchar(format[i]);
 					}
-					j++;
 				}
+
 			}
 			else
 			{
@@ -46,9 +47,38 @@ int _printf(const char *format, ...)
 			i++;
 		}
 		va_end(args);
-
 		return (n);
 	}
 
 	return (-1);
+}
+
+/**
+ * get_spec - get specifier function
+ * @s: provided format specifier
+ * @args: provided arg
+ * Return: character count
+ */
+int get_spec(char s, va_list args)
+{
+	int j = 0, n = 0;
+	spec_t specs[] = {
+		{"c", s_c},
+		{"s", s_s},
+		{"i", s_i},
+		{"d", s_d},
+		{"%", s_z},
+		{NULL, NULL}
+	};
+
+	while (specs[j].spec)
+	{
+		if (specs[j].spec[0] == s)
+		{
+			n += specs[j].f(args);
+		}
+		j++;
+	}
+
+	return (n);
 }
